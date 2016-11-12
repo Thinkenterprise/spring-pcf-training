f58530eecf87b955ec5277c38f39495c8bbf1226
Rafael Kansy <rafael.kansy@blue-sharp.de>
2016-11-12 14:26:03 +0100

    Removed @EnableCaching annotation.

--- spring-pcf-enterprise/src/main/java/com/thinkenterprise/Application.java
+++ spring-pcf-enterprise/src/main/java/com/thinkenterprise/Application.java
@@ -23,10 +23,8 @@ package com.thinkenterprise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class Application {
 
     public static void main(String[] args) {

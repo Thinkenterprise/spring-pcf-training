26fc07b80f15c47d28a7cfb9435b3930953b12a9
Rafael Kansy <rafael.kansy@blue-sharp.de>
2016-11-21 14:30:33 +0100

    Added helloWorld endpoint to Application.

--- spring-pcf-service/src/main/java/com/thinkenterprise/Application.java
+++ spring-pcf-service/src/main/java/com/thinkenterprise/Application.java
@@ -23,11 +23,20 @@ package com.thinkenterprise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/helloWorld")
+    public ResponseEntity<String> index() {
+        return ResponseEntity.ok("Hello World");
     }
 }

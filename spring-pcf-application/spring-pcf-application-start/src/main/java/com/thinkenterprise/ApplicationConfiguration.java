13ff631722daff08d39cb7cfe23107a9ec01cc39
schaefem <schaefem@MSGN04551.int.root.msg.ag>
2017-02-19 15:00:12 +0100

    Bug Fixing

--- spring-pcf-application/spring-pcf-application-start/src/main/java/com/thinkenterprise/ApplicationConfiguration.java
+++ spring-pcf-application/spring-pcf-application-start/src/main/java/com/thinkenterprise/ApplicationConfiguration.java
@@ -21,6 +21,7 @@ package com.thinkenterprise;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 import com.thinkenterprise.service.CloudEnvironmentService;
@@ -32,7 +33,7 @@ import com.thinkenterprise.service.EnvironmentService;
 public class ApplicationConfiguration {
 
 	@Bean
-	@Profile("development")
+	@Profile("default")
 	public EnvironmentService getDefaultEnvironmentService() {		
 		return new DevelopmentEnvironmentService();		
 	}
